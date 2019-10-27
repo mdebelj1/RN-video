a111e14bb9e3bd943dc3982539208a92a4c2ea73
mdebeljak <mateo.debeljak.7@gmail.com>
2019-10-27 19:49:37 +0100

    added react-native-video and sample video, initial styling

--- index.tsx
+++ index.tsx
@@ -1,9 +1,10 @@
-/**
- * @format
- */
-
+import React from 'react';
 import {AppRegistry} from 'react-native';
-import App from './App';
 import {name as appName} from './app.json';
+import {VideoPlayer} from './src/views';
+
+const App: React.FC = () => {
+  return <VideoPlayer />;
+};
 
 AppRegistry.registerComponent(appName, () => App);
---
+++ src/assets/videos/index.ts
@@ -0,0 +1,1 @@
+export {default as SampleVideo} from './SampleVideo_1280x720_1mb.mp4';
