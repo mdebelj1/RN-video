fdeb6da3f1462c585467e0b25ba8dcd840d98b04
mdebeljak <mateo.debeljak.7@gmail.com>
2019-10-27 15:19:56 +0100

    project init

---
+++ src/views/Video.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import {View, Text} from 'react-native';
+
+export default class Video extends React.Component {
+  render() {
+    return (
+      <View>
+        <Text>Video</Text>
+      </View>
+    );
+  }
+}
