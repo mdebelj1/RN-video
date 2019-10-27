aadae330d4d6f5749d9176d961d1617127ba80d8
mdebeljak <mateo.debeljak.7@gmail.com>
2019-10-27 23:44:06 +0100

    platform specific files, orientation handler for android

---
+++ src/views/VideoPlayer.android.tsx
@@ -0,0 +1,73 @@
+import React, {useState, useEffect} from 'react';
+import {
+  StyleSheet,
+  Dimensions,
+  View,
+  Text,
+  TouchableOpacity,
+  StatusBar,
+} from 'react-native';
+import Video from 'react-native-video';
+import {SampleVideo} from '../assets/videos';
+import Orientation from 'react-native-orientation-locker';
+
+export const VideoPlayer: React.FC = () => {
+  const [fullscreen, setFullscreen] = useState<boolean>(false);
+
+  useEffect(() => {
+    Orientation.addOrientationListener(handleOrientation);
+
+    return () => {
+      Orientation.removeOrientationListener(handleOrientation);
+    };
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <Video
+        source={SampleVideo}
+        style={fullscreen ? styles.fullscreenVideo : styles.video}
+        controls={true}
+        resizeMode={'contain'}
+      />
+      <Text style={styles.text}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus enim
+        suscipit ipsa impedit laboriosam saepe, sapiente excepturi molestiae
+        laudantium, non tempora cumque, quam assumenda deserunt? Similique eaque
+        voluptas itaque corporis. Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Sequi unde iusto vel facere quibusdam nisi placeat,
+        debitis veritatis autem deserunt at voluptas nam ut mollitia qui fugit
+        minus minima quod.
+      </Text>
+    </View>
+  );
+
+  function handleOrientation(orientation: string) {
+    orientation === 'LANDSCAPE-LEFT' || orientation === 'LANDSCAPE-RIGHT'
+      ? (setFullscreen(true), StatusBar.setHidden(true))
+      : (setFullscreen(false), StatusBar.setHidden(false));
+  }
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#ebebeb',
+  },
+  video: {
+    height: Dimensions.get('window').width * (9 / 16),
+    width: Dimensions.get('window').width,
+    backgroundColor: 'black',
+  },
+  fullscreenVideo: {
+    height: Dimensions.get('window').width,
+    width: Dimensions.get('window').height,
+    backgroundColor: 'black',
+  },
+  text: {
+    marginTop: 30,
+    marginHorizontal: 20,
+    fontSize: 15,
+    textAlign: 'justify',
+  },
+});
---
+++ src/views/VideoPlayer.ios.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import {StyleSheet, Dimensions, View, Text} from 'react-native';
+import Video from 'react-native-video';
+import {SampleVideo} from '../assets/videos';
+
+export const VideoPlayer: React.FC = () => {
+  return (
+    <View style={styles.container}>
+      <Video
+        source={SampleVideo}
+        style={styles.video}
+        controls={true}
+        resizeMode={'cover'}
+      />
+      <Text style={styles.text}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus enim
+        suscipit ipsa impedit laboriosam saepe, sapiente excepturi molestiae
+        laudantium, non tempora cumque, quam assumenda deserunt? Similique eaque
+        voluptas itaque corporis. Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Sequi unde iusto vel facere quibusdam nisi placeat,
+        debitis veritatis autem deserunt at voluptas nam ut mollitia qui fugit
+        minus minima quod.
+      </Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#ebebeb',
+  },
+  video: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').width * (9 / 16),
+    backgroundColor: 'black',
+  },
+  text: {
+    marginTop: 30,
+    marginHorizontal: 20,
+    fontSize: 15,
+    textAlign: 'justify',
+  },
+});
