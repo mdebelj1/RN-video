eb6368b374ce9e3f1c3fc7676cf0afce2d92f2f2
mdebeljak <mateo.debeljak.7@gmail.com>
2019-10-28 20:40:37 +0100

    fullscreen button style and function

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,2 @@
+export {default as FullscreenOpen} from './fullscreen-open.svg';
+export {default as FullscreenClose} from './fullscreen-close.svg';
--- src/views/VideoPlayer.android.tsx
+++ src/views/VideoPlayer.android.tsx
@@ -10,6 +10,7 @@ import {
 import Video from 'react-native-video';
 import {SampleVideo} from '../assets/videos';
 import Orientation from 'react-native-orientation-locker';
+import {FullscreenClose, FullscreenOpen} from '../assets/icons';
 
 export const VideoPlayer: React.FC = () => {
   const [fullscreen, setFullscreen] = useState<boolean>(false);
@@ -24,12 +25,19 @@ export const VideoPlayer: React.FC = () => {
 
   return (
     <View style={styles.container}>
-      <Video
-        source={SampleVideo}
-        style={fullscreen ? styles.fullscreenVideo : styles.video}
-        controls={true}
-        resizeMode={'contain'}
-      />
+      <View>
+        <Video
+          source={SampleVideo}
+          style={fullscreen ? styles.fullscreenVideo : styles.video}
+          controls={true}
+          resizeMode={'contain'}
+        />
+        <TouchableOpacity
+          onPress={handleFullscreen}
+          style={styles.fullscreenButton}>
+          {fullscreen ? <FullscreenClose /> : <FullscreenOpen />}
+        </TouchableOpacity>
+      </View>
       <Text style={styles.text}>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus enim
         suscipit ipsa impedit laboriosam saepe, sapiente excepturi molestiae
@@ -46,6 +54,12 @@ export const VideoPlayer: React.FC = () => {
     orientation === 'LANDSCAPE-LEFT' || orientation === 'LANDSCAPE-RIGHT'
       ? (setFullscreen(true), StatusBar.setHidden(true))
       : (setFullscreen(false), StatusBar.setHidden(false));
+  }
+
+  function handleFullscreen() {
+    fullscreen
+      ? Orientation.unlockAllOrientations()
+      : Orientation.lockToLandscapeLeft();
   }
 };
 
@@ -69,5 +83,12 @@ const styles = StyleSheet.create({
     marginHorizontal: 20,
     fontSize: 15,
     textAlign: 'justify',
+  },
+  fullscreenButton: {
+    position: 'absolute',
+    top: 0,
+    right: 0,
+    backgroundColor: 'red',
+    padding: 10,
   },
 });
